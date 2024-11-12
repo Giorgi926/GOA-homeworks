e36dfbacc57fb770c5e056e9821b58ecb20cd17d
Giorgi926 <marigiozuradati@gmail.com>
2024-11-12 17:02:32 +0400

    second commit

---
+++ level04/classwork/classwork.py
@@ -0,0 +1,25 @@
+str1 = "giorgi"
+str2 = "levani"
+str3 = "gurami"
+str4 = "akaki"
+
+int1 = (10)
+int2 = (20)
+int3 = (30)
+int4 = (40)
+
+float1 = (1,5)
+float2 = (2,5)
+float3 = (3,5)
+float4 = (4,5)
+
+print(str1 + str2 + str3 + str4)
+print(int1 + int2 + int3 + int4)
+print(float1 + float2 + float3 + float4)
+
+print(5 + 5)
+print(5 - 5)
+print(5 * 5)
+print(5 ** 5)
+print(5 / 5)
+print(5 // 5)
---
+++ level04/homework/homework.py
@@ -0,0 +1,55 @@
+# დავალება 2
+# შეკრება
+print(10 + 15)
+print(20 + 42)
+print(50 + 50)
+
+# გამოკლება
+print(100 - 99)
+print(50 - 35)
+print(70 - 30)
+
+# გამრავლება
+print(20 * 20)
+print(10 * 8)
+print(22 * 3)
+
+#გაყოფა
+print(20 / 2)
+print(50 / 10)
+print(60 / 3)
+
+#პროცენტული გაყოფა
+print(22 % 12)
+print(40 % 27)
+print(36 % 17)
+
+# ორი გამრავლება
+a = 10
+b = 2
+result = a ** b
+print("a ** b = ", result)
+
+a = 20
+b = 2
+cum = a ** b
+print("a ** b = ", cum)
+
+a = 40
+b = 2
+number = a ** b
+print("a ** b = ", number)
+
+#დავალება 3
+print(50 / 2) # ჩვეულებრივი გაყოფა რიცხვს ყოფს უნაშთოდ
+
+print(71 % 28) # პროცენტულ გაყოფას გამოაქ ნაშთი
+
+age1 = 14
+age2 = 21
+age3 = 71
+
+# დავალება 4
+print("chemi saxelia giorgi devsurashvili" , age1 , "wlis")
+print("chemi dis saxelia mariami" , age2 , "wlis")
+print("bebiachemis saxelia gulo" , age3 , "wlis")
