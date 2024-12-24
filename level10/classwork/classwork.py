1443ccc7e972170f721549fbd22b28c4ddfe99c4
Giorgi926 <marigiozuradati@gmail.com>
2024-12-24 17:14:33 +0400

    commit

---
+++ level10/classwork/classwork.py
@@ -0,0 +1,10 @@
+name = (input("Enter your name: "))
+surname = (input("Enter your usarname: "))
+age = int(input("Enter your age: "))
+number1 = int(input("Enter number1: "))
+number2 = int(input("Enter number2: "))
+
+num1 = int(input("Enter your first number: "))
+num2 = int(input("enter your second number: "))
+print(num1 + num2)
+        
---
+++ level10/homework/homework.py
@@ -0,0 +1,49 @@
+name = input("enter your name :")
+age = int(input("enter your age :"))
+
+print("your name is" + name + "your age is" + str(age))
+
+# 2)
+my = int(input("enter my age: "))
+mom = int(input("enter mom age: "))
+sister = int(input("enter sister age: "))
+dad = int(input("enter dad age: "))
+
+my_age_in_ten_years = my + 10
+ 
+mom_age_in_ten_years = mom + 10
+
+sister_age_in_ten_years = sister + 10
+
+dad_age_in_ten_years = dad + 10
+
+print(my_age_in_ten_years)
+print(mom_age_in_ten_years)
+print(sister_age_in_ten_years)
+print(dad_age_in_ten_years)
+
+# 3)
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+
+# 4)
+num1 = 5
+num2 = 10
+num3 = 15
+num4 = 20
+num5 = 25
+
+print(num1 + num5)
+print(num2 - num1)
+print(num3 / num1)
+print(num1 % num2)
+print(num4 * num5)
+print(num3 ** num1)
+print(num1 + num2)
+print(num5 - num1)
+print(num4 * num2)
+print(num4 / num1)
