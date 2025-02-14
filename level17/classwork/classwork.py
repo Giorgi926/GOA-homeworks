ede37767829f870fbdd5af71164b6d878a7a3981
Giorgi926 <marigiozuradati@gmail.com>
2025-02-14 18:31:02 +0400

    commit

---
+++ level17/classwork/classwork.py
@@ -0,0 +1,14 @@
+for i in range(950,1000,2):
+    print(i)
+
+#classwork 2
+for i in range(100):
+    print("giorgi")
+
+#classwork 3
+for i in range(1000):
+    print("goa is the best academy")
+
+#classwork 4
+for i in range(100,200):
+    print(i)
---
+++ level17/homework/homework.py
@@ -0,0 +1,23 @@
+#homework 1
+for i in range(300,451,10):
+    print(i)
+
+#homework 2
+for i in range(200,500,2):
+    print(i)
+
+#homework 3
+for i in range(0,50,3):
+    print(i)
+
+#homework 4
+for i in range(1000,1101,2):
+    print(i)
+
+#homework 5
+for i in range(1,10):
+    print(i)
+
+#homework 6
+for i in range(2000,2021):
+    print(i)
