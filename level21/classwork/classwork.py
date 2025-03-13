3739963ee718669a2dd45096d3c71c3848f3b9fc
Giorgi926 <marigiozuradati@gmail.com>
2025-03-13 22:14:22 +0400

    commit

---
+++ level21/classwork/classwork.py
@@ -0,0 +1,26 @@
+#                    0          1           2          3        4
+favorite_cars = ["Buggatti", "Ferrari", "Lamborgini", "BMW", "Mersedes"]
+print(favorite_cars)
+
+print(favorite_cars[0])
+
+#                    0             1           2
+favorite_food = ["Khinkali", "Khachapuri", "Mwvadi"]
+print(favorite_food)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+print(numbers[7])
+print(numbers[8])
+print(numbers[9])
+
+print(numbers[1:8])
+print(numbers[4:])
---
+++ level21/homework/homework.py
@@ -0,0 +1,28 @@
+# 1)
+
+numbers = []
+
+for i in range(1, 10 + 1):
+    numbers.append(i)
+
+print(numbers)
+
+# 2)
+
+# 0,1,2,3,4
+
+numbers = []
+
+for i in range(0, 5):
+    numbers.append(i)
+
+print(numbers)
+
+# 3)
+
+numbers = []
+
+for i in range(4, 7 + 1):
+    numbers.append(i)
+
+print(numbers)
