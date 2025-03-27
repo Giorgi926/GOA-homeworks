fe3fb89bc3b63bcf6dae95920e31774d6581f587
Giorgi926 <marigiozuradati@gmail.com>
2025-03-27 16:53:16 +0400

    commit

---
+++ level23/classwork.py
@@ -0,0 +1,2 @@
+thislist = ["apple", "banana", "cherry" , "orange" , "pinaple" , "grape" , "tangerine" , "coconate" , "dragon fruit" , "kiwi"]
+print(len(thislist))
---
+++ level23/homework.py
@@ -0,0 +1,6 @@
+#davaleba 1
+thislist = ["apple", "banana", "cherry" , "orange" , "pinaple" , "grape" , "tangerine" , "coconate" , "dragon fruit" , "kiwi"]
+print(len(thislist))
+
+#list-ლისტი არის სია სადაც სჰეგილია ნებისმიერი რიცხვი,ხილი, ან რაიმე სხვა დაპრინტო
+#სიის თითოეულ პუნქტს შეესაბამება ინდექსის ნომერი, რომელიც არის მთელი რიცხვი, დაწყებული ინდექსის ნომრით 0
